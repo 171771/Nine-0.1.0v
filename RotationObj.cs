28b3c72e5fb899d14004724e03188231e492cf6b
Seoneem <bea0631@gmail.com>
2016-03-12 15:17:32 +0900

    mouseorbitImproved 삭제

---
+++ RotationObj.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationObj : MonoBehaviour {
+    private float angleY;
+    private float angleX;
+    private float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // right rotation
+        if (Input.GetKey(""))
+        {
+            angleY += 200 * Time.deltaTime * speed;
+            transform.eulerAngles = new Vector3(0, angleY, 0);
+        }
+        // left rotation
+        if (Input.GetKey(""))
+        {
+            angleY += 200 * Time.deltaTime * speed;
+            transform.eulerAngles = new Vector3(0, angleY, 0);
+        }
+        // up rotation
+        if (Input.GetKey(""))
+        {
+            angleX += 200 * Time.deltaTime * speed;
+            transform.eulerAngles = new Vector3(angleX, 0, 0);
+        }
+
+        // down rotation
+        if (Input.GetKey(""))
+        {
+            angleX += 200 * Time.deltaTime * speed;
+            transform.eulerAngles = new Vector3(angleX, 0, 0);
+        }
+    }
+}
